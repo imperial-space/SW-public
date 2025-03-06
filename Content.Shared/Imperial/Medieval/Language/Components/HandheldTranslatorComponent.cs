891a2b18c8875cfe1284f82df44d8a3db685297c
FaDeOkno <logkedr18@gmail.com>
2025-03-06 18:14:21 +0400

    add(src): Основные компоненты

---
+++ Content.Shared/Imperial/Medieval/Language/Components/HandheldTranslatorComponent.cs
@@ -0,0 +1,35 @@
+using Content.Shared.Actions;
+using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.ADT.Language;
+
+/// <summary>
+/// Компонент, дающий знание языков, когда находится в руках и активен
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
+public sealed partial class HandheldTranslatorComponent : Component
+{
+    /// <summary>
+    /// Список языков, которые даёт переводчик. Писать в компонентах как:
+    /// Прототип: Understand/BadSpeak/Speak
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("languages", required: true), AutoNetworkedField]
+    public Dictionary<string, LanguageKnowledge> Languages;
+
+    /// <summary>
+    /// Будет ли переключаться переводчик по взаимодействию с ним
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("toggle")]
+    public bool ToggleOnInteract = true;
+
+    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [DataField]
+    public bool Enabled = false;
+
+    public EntityUid? User;
+}
---
+++ Content.Shared/Imperial/Medieval/Language/Components/LanguageSpeakerComponent.cs
@@ -0,0 +1,38 @@
+using Robust.Shared.Prototypes;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.ADT.Language;
+
+/// <summary>
+/// Данный компонент позволяет сущности понимать и говорить на языках
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class LanguageSpeakerComponent : Component
+{
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [AutoNetworkedField]
+    public string? CurrentLanguage = default!;
+
+    /// <summary>
+    /// Список языков, которые знает сущность. Писать в компонентах как:
+    /// Прототип: Understand/BadSpeak/Speak
+    /// </summary>
+    [DataField("languages"), ViewVariables(VVAccess.ReadWrite)]
+    [AutoNetworkedField]
+    public Dictionary<string, LanguageKnowledge> Languages = new();
+}
+
+/// <summary>
+/// Уровень владения языком.
+/// <see cref="Understand"/> позволяет сущности понимать язык, но не говорить на нём
+/// <see cref="BadSpeak"/> позволяет сущности говорить на языке, однако при этом подпарчивает текст. Как эффект от алкоголя, но без рыганий
+/// <see cref="Speak"/> позволяет сущности свободно говорить на языке
+/// </summary>
+[Serializable, NetSerializable]
+public enum LanguageKnowledge : int
+{
+    Understand = 0,
+    BadSpeak = 1,
+    Speak = 2
+}
