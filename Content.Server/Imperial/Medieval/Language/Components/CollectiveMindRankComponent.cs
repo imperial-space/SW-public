c71bef028580fa9363159b1d9b9a8c1faf82cda3
FaDeOkno <logkedr18@gmail.com>
2025-03-06 18:26:05 +0400

    add(src): Коллективный разум как язык

---
+++ Content.Server/Imperial/Medieval/Language/Components/CollectiveMindRankComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Server.ADT.Language;
+
+[RegisterComponent]
+public sealed partial class CollectiveMindRankComponent : Component
+{
+    [DataField]
+    public string RankName = "???";
+}
