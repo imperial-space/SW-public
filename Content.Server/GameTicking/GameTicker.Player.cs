f903fb3e946c9888e496777e77ac03c2a7f8cdc4
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2025-03-24 01:13:30 +1000

    fix: Remove offical spawn method

--- Content.Server/GameTicking/GameTicker.Player.cs
+++ Content.Server/GameTicking/GameTicker.Player.cs
@@ -57,7 +57,9 @@ namespace Content.Server.GameTicking
 
                     // Make the player actually join the game.
                     // timer time must be > tick length
-                    Timer.Spawn(0, () => _playerManager.JoinGame(args.Session));
+                    // Imperial-Medieval-JoinQueue-Start
+                    // Timer.Spawn(0, () => _playerManager.JoinGame(args.Session));
+                    // Imperial-Medieval-JoinQueue-End
 
                     var record = await _db.GetPlayerRecordByUserId(args.Session.UserId);
                     var firstConnection = record != null &&
