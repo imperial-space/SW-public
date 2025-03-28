dc0c3a903cdce586c1c44703bbde155f0a1457d6
dantanat1 <93005892+dantanat1@users.noreply.github.com>
2025-03-28 20:53:03 +0300

    зверолюди баф

---
+++ Content.Shared/Imperial/Medieval/RandomSteal/StealRaceChanceIncreaserComponent.cs
@@ -0,0 +1,6 @@
+namespace Content.Shared.Imperial.RandomSteal.Components;
+[RegisterComponent]
+public sealed partial class StealRaceChanceIncreaserComponent : Component {
+    [DataField]
+    public int Bonus = 8;
+ }
