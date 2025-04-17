fe89cd2656830659db3bed2804dc3d6f89c42097
FaDeOkno <logkedr18@gmail.com>
2025-04-17 16:23:45 +0400

    add(src): Серверная часть системы

---
+++ Content.Server/Imperial/Medieval/Skills/Systems/SkillsSystem.cs
@@ -0,0 +1,19 @@
+using Content.Shared.Imperial.Medieval.Skills;
+
+namespace Content.Server.Imperial.Medieval.Skills;
+
+public sealed partial class SkillsSystem : SharedSkillsSystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        InitializeAgility();
+
+        SubscribeLocalEvent<SkillsComponent, SkillLevelChangedEvent>(OnLevelChanged);
+    }
+
+    private void OnLevelChanged(EntityUid uid, SkillsComponent comp, ref SkillLevelChangedEvent args)
+    {
+        VitalityLevelSet(uid, args.Level, args.OldLevel);
+    }
+}
