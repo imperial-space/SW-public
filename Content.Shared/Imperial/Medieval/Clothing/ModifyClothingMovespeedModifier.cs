85b3bc5946b0b5ca0428f5096706075210d20bbe
FaDeOkno <logkedr18@gmail.com>
2025-04-17 17:46:00 +0400

    style(src): Ренеймы ивента в более корректную форму

--- Content.Shared/Imperial/Medieval/Clothing/ModifyClothingMovespeedModifier.cs
+++
@@ -1,4 +0,0 @@
-namespace Content.Shared.Imperial.Medieval.Clothing;
-
-[ByRefEvent]
-public record struct ModifyClothingMovespeedModifier(float WalkMod, float SprintMod, float Walk = 0, float Sprint = 0);
