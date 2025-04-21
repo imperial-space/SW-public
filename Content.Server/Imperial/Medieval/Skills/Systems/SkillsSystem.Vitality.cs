dffe52842075a353a2e9c89b5def8d9295faabec
FaDeOkno <logkedr18@gmail.com>
2025-04-21 18:03:54 +0400

    add(src): Мелкие фиксы и события с таймером

--- Content.Server/Imperial/Medieval/Skills/Systems/SkillsSystem.Vitality.cs
+++ Content.Server/Imperial/Medieval/Skills/Systems/SkillsSystem.Vitality.cs
@@ -1,7 +1,14 @@
+using Content.Server.Body.Components;
+using Content.Server.Body.Systems;
+using Content.Server.Chat.Systems;
 using Content.Server.Imperial.Medieval.Body;
 using Content.Server.Imperial.Medieval.NeedSleep;
+using Content.Server.Popups;
 using Content.Shared.Imperial.Medieval.Skills;
 using Content.Shared.Mobs;
+using Content.Shared.Movement.Pulling.Components;
+using Robust.Server.GameObjects;
+using Robust.Shared.Physics.Components;
 
 namespace Content.Server.Imperial.Medieval.Skills;
 
@@ -17,6 +24,9 @@ public sealed partial class SkillsSystem
 
     private void OnGetSleepModifiers(EntityUid uid, SkillsComponent comp, ref GetSleepLevelModifiersEvent args)
     {
+        if (!args.Sleeping)
+            return;
+
         var (proto, level) = GetSkill(uid, VitalityId);
 
         if (level == 10)
@@ -53,6 +63,27 @@ public sealed partial class SkillsSystem
         var (proto, _) = GetSkill(uid, VitalityId);
 
         var diff = level - oldLevel;
+
+        Comp<SkillsComponent>(uid).Timers.Remove(VitalityId);
+        if (level <= 1)
+            Comp<SkillsComponent>(uid).Timers.Add(VitalityId, _timing.CurTime + TimeSpan.FromSeconds(30f));
+
+        if (TryComp<SoftCritEmotesComponent>(uid, out var crit))
+        {
+            crit.MinDamage += proto.Modifiers["AliveHealthPerLevel"] * diff;
+            crit.Emote = level > 16;
+        }
+        if (TryComp<PullerComponent>(uid, out var puller))
+        {
+            puller.PseudoMass += proto.Modifiers["PseudoMassModifier"] * diff;
+            Dirty(uid, puller);
+        }
+        if (TryComp<PullableComponent>(uid, out var pullable))
+        {
+            pullable.PseudoMass += proto.Modifiers["PseudoMassModifier"] * diff;
+            Dirty(uid, pullable);
+        }
+
         _threshold.SetMobStateThreshold(uid,
                                         _threshold.GetThresholdForState(uid, MobState.Alive) + proto.Modifiers["AliveHealthPerLevel"] * diff,
                                         MobState.Alive);
@@ -86,5 +117,35 @@ public sealed partial class SkillsSystem
         // _threshold.SetMobStateThreshold(uid,
         //                                 woundedThreshold,
         //                                 MobState.Dead);
+    }
+
+    private void UpdateVitality(float frameTime)
+    {
+        var query = EntityQueryEnumerator<SkillsComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (comp.Timers.TryGetValue(VitalityId, out var timer) || _timing.CurTime < timer)
+                continue;
+
+            if (GetSkill(uid, VitalityId).Item2 > 1)
+                continue;
+
+            comp.Timers[VitalityId] = _timing.CurTime + TimeSpan.FromSeconds(_random.NextFloat(30f, 60f));
+
+            var i = _random.Next(0, 3);
+            switch (i)
+            {
+                case 0:
+                    _chat.TrySendInGameICMessage(uid, "кряхтит", InGameICChatType.Emote, false);
+                    break;
+                case 1:
+                    _popup.PopupEntity("Спина болит...", uid, uid, Shared.Popups.PopupType.SmallCaution);
+                    break;
+                case 2:
+                    _popup.PopupEntity("Ух, как же тяжело...", uid, uid, Shared.Popups.PopupType.SmallCaution);
+                    break;
+            };
+
+        }
     }
 }
