c49789e773e400b4b997bec82b9da216d8b02b44
FaDeOkno <logkedr18@gmail.com>
2025-05-08 02:50:51 +0400

    tweak(src): Поддержка локали

--- Content.Server/Imperial/Medieval/WarningOnAttach/WarningOnAttachComponent.cs
+++ Content.Server/Imperial/Medieval/WarningOnAttach/WarningOnAttachComponent.cs
@@ -3,8 +3,10 @@ namespace Content.Server.Imperial.Medieval.WarningOnAttach;
 [RegisterComponent]
 public sealed partial class WarningOnAttachComponent : Component
 {
-    [DataField]
-    public string Message = "";
+    public string Message => Loc.GetString(_message);
+
+    [DataField("message")]
+    private string _message = "";
 
     public List<string> Players = new();
 }
