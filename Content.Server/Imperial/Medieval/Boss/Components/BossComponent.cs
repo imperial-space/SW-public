a7cd2b3f926e1b93deaf9d92b98fb1be1aa81e8f
FaDeOkno <logkedr18@gmail.com>
2025-06-27 22:25:14 +0400

    add(src): Основные компоненты

---
+++ Content.Server/Imperial/Medieval/Boss/Components/BossComponent.cs
@@ -0,0 +1,25 @@
+using System.Threading;
+
+namespace Content.Server.Imperial.Medieval.Boss;
+
+[RegisterComponent]
+public sealed partial class BossComponent : Component
+{
+    [DataField]
+    public bool Active = false;
+
+    [DataField(required: true)]
+    public Dictionary<int, BossStageData> Stages = new();
+
+    [DataField(required: true)]
+    public float Health = 100f;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public int Stage = 0;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public List<EntityUid> Players = new List<EntityUid>();
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan NextAttack = TimeSpan.Zero;
+}
