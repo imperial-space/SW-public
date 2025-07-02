0ac2b9a40a69a11014d3fbbc365f7553a10b465d
dantanat1 <93005892+dantanat1@users.noreply.github.com>
2025-07-02 03:34:34 +0300

    фикс старт-щита, мирмексовых штук

--- Content.Shared/Imperial/Medieval/Additions/ShieldOnStartupSystem.cs
+++ Content.Shared/Imperial/Medieval/Additions/ShieldOnStartupSystem.cs
@@ -4,6 +4,7 @@ using Content.Shared.Damage;
 using Content.Shared.Damage.Systems;
 using Robust.Shared.Timing;
 using Content.Shared.Damage.Events;
+using Content.Shared.Rejuvenate;
 
 namespace Content.Shared.Imperial.Medieval.Additions;
 
@@ -16,6 +17,13 @@ public partial class ShieldOnStartupSystem : EntitySystem
         SubscribeLocalEvent<ShieldOnStartupComponent, ComponentStartup>(Init);
         SubscribeLocalEvent<ShieldOnStartupComponent, BeforeDamageChangedEvent>(OnBeforeDamageChanged);
         SubscribeLocalEvent<ShieldOnStartupComponent, BeforeStaminaDamageEvent>(OnBeforeStaminaDamage);
+        SubscribeLocalEvent<ShieldOnStartupComponent, RejuvenateEvent>(OnRejuv);
+    }
+
+    public void OnRejuv(EntityUid uid, ShieldOnStartupComponent component, RejuvenateEvent args)
+    {
+        component.Spawned += TimeSpan.FromSeconds(45);
+        _alert.ShowAlert(uid, "SpawnProtection", null, (_tick.CurTime, _tick.CurTime ), true);
     }
     public void Init(EntityUid uid, ShieldOnStartupComponent component, ComponentStartup args)
     {
@@ -24,13 +32,13 @@ public partial class ShieldOnStartupSystem : EntitySystem
     }
     private void OnBeforeDamageChanged(EntityUid uid, ShieldOnStartupComponent component, ref BeforeDamageChangedEvent args)
     {
-        if (component.Spawned + TimeSpan.FromSeconds(90) < _tick.CurTime)
+        if (component.Spawned + TimeSpan.FromSeconds(45) < _tick.CurTime)
             return;
         args.Cancelled = true;
     }
     private void OnBeforeStaminaDamage(EntityUid uid, ShieldOnStartupComponent component, ref BeforeStaminaDamageEvent args)
     {
-        if (component.Spawned + TimeSpan.FromSeconds(90) < _tick.CurTime)
+        if (component.Spawned + TimeSpan.FromSeconds(45) < _tick.CurTime)
             return;
         args.Cancelled = true;
     }
