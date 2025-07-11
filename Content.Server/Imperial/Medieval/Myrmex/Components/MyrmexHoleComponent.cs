228f17ba51b9fd65a9acb164dc7e199c1844f76b
dantanat1 <93005892+dantanat1@users.noreply.github.com>
2025-07-12 02:07:12 +0300

    насрал в лестницу

---
+++ Content.Server/Imperial/Medieval/Myrmex/Components/MyrmexHoleComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.Myrmex.Components
+{
+    [RegisterComponent]
+    public sealed partial class MyrmexHoleComponent : Component
+    {
+        [DataField]
+        public bool Entrance = true;
+
+    }
+}
