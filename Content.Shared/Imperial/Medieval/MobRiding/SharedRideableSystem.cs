bf48eed98e0654c50eeaac2536e619cc44ca3c13
Ligyb <65973111+Lgibb18@users.noreply.github.com>
2025-07-11 18:10:54 +0500

    server

--- Content.Shared/Imperial/Medieval/MobRiding/SharedRideableSystem.cs
+++ Content.Shared/Imperial/Medieval/MobRiding/SharedRideableSystem.cs
@@ -41,17 +41,6 @@ namespace Content.Shared.Imperial.Medieval.MobRiding
         [Dependency] private readonly SharedAudioSystem _audio = default!;
         #endregion
 
-        #region Const
-        private readonly string[] _clashOne = new[] {
-            "/Audio/Imperial/Medieval/clash_one1.ogg",
-            "/Audio/Imperial/Medieval/clash_one2.ogg",
-        };
-
-        private readonly string _clashBoth = "/Audio/Imperial/Medieval/clash_both.ogg";
-        private readonly string _clashNone = "/Audio/Imperial/Medieval/wood_parry.ogg";
-
-        #endregion
-
         #region Initialize
         public override void Initialize()
         {
@@ -63,12 +52,6 @@ namespace Content.Shared.Imperial.Medieval.MobRiding
 
             SubscribeLocalEvent<RideableComponent, StrappedEvent>(OnBuckled);
             SubscribeLocalEvent<RideableComponent, UnstrappedEvent>(OnUnbuckled);
-
-            // TODO: поменять на PikeComponent
-            SubscribeLocalEvent<SpearComponent, ItemWieldedEvent>(OnWielded);
-            SubscribeLocalEvent<SpearComponent, ItemUnwieldedEvent>(OnUnwielded);
-
-            SubscribeLocalEvent<RideableComponent, StartCollideEvent>(HandleCollide);
 
         }
         #endregion
@@ -118,293 +101,6 @@ namespace Content.Shared.Imperial.Medieval.MobRiding
                 return false;
 
             return true;
-        }
-
-        #endregion
-
-        #region Pikes
-
-        private void HandleCollide(EntityUid uid, RideableComponent comp, ref StartCollideEvent args)
-        {
-            if (args.OurFixtureId != comp.PikeShapeId)
-                return;
-
-            if (args.OtherEntity == comp.Rider)
-                return;
-
-            Logger.Debug(args.OurFixtureId);
-            Logger.Debug(args.OtherFixtureId);
-            Logger.Debug(args.OtherEntity.Id.ToString());
-
-            if (!TryComp<MobStateComponent>(args.OtherEntity, out var mobState))
-                return;
-
-            if (mobState.CurrentState != MobState.Alive)
-                return;
-
-            if(args.OtherFixtureId != comp.PikeShapeId)
-                PikeHitEntity(uid, comp, args.OtherEntity);
-            else
-                PikeClash(uid, comp, args.OtherEntity);
-        }
-
-        private void PikeHitEntity(EntityUid uid, RideableComponent comp, EntityUid other, uint stunSeconds = 2, bool throwOther = true)
-        {
-            Logger.Debug("Hit entity");
-            if (!TryComp<PhysicsComponent>(uid, out var rideablePhysics))
-                return;
-
-            if (!comp.Rider.HasValue)
-                return;
-
-            if (!TryGetPike(comp.Rider.Value, out var item, out var melee))
-                return;
-
-            if (TryComp<UseDelayComponent>(item, out var useDelay) && _useDelay.IsDelayed((item.Value, useDelay)))
-                return;
-
-
-            var direction = rideablePhysics.LinearVelocity;
-
-            if(throwOther)
-                _throwing.TryThrow(other, direction * 0.6f);
-            if(stunSeconds > 0)
-                _stun.TryKnockdown(other, TimeSpan.FromSeconds(stunSeconds), true);
-
-            _damageable.TryChangeDamage(other, melee.Damage);
-
-            if (useDelay != null)
-                _useDelay.TryResetDelay((item.Value, useDelay));
-        }
-
-        private bool TryGetPike(EntityUid uid, [NotNullWhen(true)] out EntityUid? pike, [NotNullWhen(true)] out MeleeWeaponComponent? comp)
-        {
-            pike = null;
-            comp = null;
-
-            pike = _hands.GetActiveItem(uid);
-            return pike.HasValue && TryComp(pike, out comp);
-        }
-
-        private sealed class Skills
-        {
-            public int Intelligence;
-            public int Endurance;
-            public int Agility;
-            public int Strength;
-            public int Vitality;
-
-            public Skills(SharedRideableSystem system, EntityUid uid)
-            {
-                system.TryGetSkill(uid, "Intelligence", out Intelligence);
-                system.TryGetSkill(uid, "Endurance", out Endurance);
-                system.TryGetSkill(uid, "Agility", out Agility);
-                system.TryGetSkill(uid, "Strength", out Strength);
-                system.TryGetSkill(uid, "Vitality", out Vitality);
-            }
-        }
-
-        private bool TryGetClashData(EntityUid uid,
-            [NotNullWhen(true)] out RideableComponent? comp,
-            [NotNullWhen(true)] out EntityUid? rider,
-            out float attackStrength,
-            out float stability,
-            out float finalAttack,
-            out float finalStability)
-        {
-            comp = null;
-            rider = null;
-            attackStrength = 0;
-            stability = 0;
-            finalAttack = 0;
-            finalStability = 0;
-
-
-            if (!TryComp(uid, out comp))
-                return false;
-
-            if (!comp.Rider.HasValue)
-                return false;
-
-            rider = comp.Rider;
-
-            if (!HasComp<SkillsComponent>(rider))
-                return false;
-
-            var skills = new Skills(this, rider.Value);
-
-            attackStrength = (skills.Strength * 1.4f) + (skills.Agility * 1.0f) + (skills.Endurance * 0.6f);
-            stability = (skills.Vitality * 1.6f) + (skills.Agility * 1.15f) + (skills.Strength * 0.3f);
-
-            finalAttack = attackStrength + _random.Next(1, 21);
-            finalStability = stability + _random.Next(1, 21);
-
-            return true;
-        }
-
-        private CheckResult ResolveHitOutcome(float finalAttack, float otherFinalAttack)
-        {
-            var difference = finalAttack - otherFinalAttack;
-
-            return difference switch
-            {
-                > 10 => CheckResult.Self,
-                < -10 => CheckResult.Other,
-                _ => CheckResult.Draw
-            };
-        }
-
-        private CheckResult ResolveStabilityOutcome(float finalStability,
-            float otherFinalStability,
-            float stabilityDifficulty)
-        {
-            var selfPasses = finalStability >= stabilityDifficulty;
-            var otherPasses = otherFinalStability >= stabilityDifficulty;
-
-            return (selfPasses, otherPasses) switch
-            {
-                (true, true) => CheckResult.Both,
-                (false, false) => CheckResult.Draw,
-                (true, false) => CheckResult.Self,
-                (false, true) => CheckResult.Other
-            };
-        }
-
-        private void ThrowClash(EntityUid rideable, EntityUid rider, EntityUid otherRider)
-        {
-            if (!TryComp<PhysicsComponent>(rideable, out var rideablePhysics))
-                return;
-            var velocity = rideablePhysics.AngularVelocity;
-            if (!TryGetPike(rider, out var pike, out var pikeComp))
-                return;
-            ThrowFromRideable(otherRider, 2, pikeComp.Damage * velocity);
-        }
-
-
-        private void PikeClash(EntityUid uid, RideableComponent comp, EntityUid other)
-        {
-            Logger.Debug("Hit clash");
-
-            if (uid.Id < other.Id)
-                return;
-
-            if (!TryGetClashData(uid,
-                    out _,
-                    out var rider,
-                    out var attackStrength,
-                    out var stability,
-                    out var finalAttack,
-                    out var finalStability))
-                return;
-
-            if (!TryGetClashData(other,
-                    out var otherComp,
-                    out var otherRider,
-                    out var otherAttackStrength,
-                    out var otherStability,
-                    out var otherFinalAttack,
-                    out var otherFinalStability))
-                return;
-
-            var hitResult = ResolveHitOutcome(finalAttack, otherFinalAttack);
-
-            switch (hitResult)
-            {
-                case CheckResult.Self:
-                    ThrowClash(uid, rider.Value, otherRider.Value);
-                    break;
-                case CheckResult.Other:
-                    ThrowClash(other, otherRider.Value, rider.Value);
-                    break;
-                case CheckResult.Draw:
-                    var stabilityDifficulty = (finalAttack + otherFinalAttack) / 2;
-                    var stabilityResult =
-                        ResolveStabilityOutcome(finalStability, otherFinalStability, stabilityDifficulty);
-
-                    var oneSound = _random.Pick(_clashOne);
-                    switch (stabilityResult)
-                    {
-                        case CheckResult.Self:
-                            _audio.PlayPvs(oneSound, uid);
-                            ThrowClash(uid, rider.Value, otherRider.Value);
-                            break;
-                        case CheckResult.Other:
-                            _audio.PlayPvs(oneSound, other);
-                            ThrowClash(other, otherRider.Value, rider.Value);
-                            break;
-                        case CheckResult.Draw:
-                            _audio.PlayPvs(_clashBoth, uid);
-                            ThrowClash(uid, rider.Value, otherRider.Value);
-                            ThrowClash(other, otherRider.Value, rider.Value);
-                            break;
-                        case CheckResult.Both:
-                            _audio.PlayPvs(_clashNone, uid);
-                            break;
-                    }
-                    break;
-            }
-        }
-
-        private bool CheckPike(EntityUid uid, out EntityUid? item)
-        {
-            item = _hands.GetActiveItem(uid);
-
-            if (!item.HasValue)
-                return false;
-
-            //TODO: изменить на PikeComponent
-            if (!HasComp<SpearComponent>(item.Value))
-                return false;
-
-            if (!TryComp<WieldableComponent>(item.Value, out var wieldable))
-                return false;
-
-            return wieldable.Wielded;
-        }
-
-        // TODO: поменять на PikeComponent
-        private void OnWielded(EntityUid uid, SpearComponent comp, ref ItemWieldedEvent args)
-        {
-            var rider = args.User;
-
-            if (!TryGetRideable(rider, out var rideableEntity, out var rideable))
-                return;
-
-            if (rideable.Pike != null)
-                return;
-
-            rideable.Pike = uid;
-            rideable.Dirty();
-
-            CreatePikeFixture(rideableEntity.Value, rideable);
-        }
-
-        // TODO: это тоже
-        private void OnUnwielded(EntityUid uid, SpearComponent comp, ref ItemUnwieldedEvent args)
-        {
-            var rider = args.User;
-
-            if (!TryGetRideable(rider, out var rideableEntity, out var rideable))
-                return;
-
-            if (rideable.Pike != uid)
-                return;
-
-            rideable.Pike = null;
-            rideable.Dirty();
-
-            RemovePikeFixture(rideableEntity.Value, rideable);
-        }
-
-        private void CreatePikeFixture(EntityUid uid, RideableComponent rideable)
-        {
-            _fixtureSystem.TryCreateFixture(uid, rideable.PikeShape, rideable.PikeShapeId, density: 1, collisionLayer: (int)CollisionGroup.PikeLayer, collisionMask: (int)CollisionGroup.PikeMask, hard:false);
-        }
-
-        private void RemovePikeFixture(EntityUid uid, RideableComponent rideable)
-        {
-            Logger.Debug("removing");
-            _fixtureSystem.DestroyFixture(uid, rideable.PikeShapeId);
         }
 
         #endregion
@@ -479,11 +175,6 @@ namespace Content.Shared.Imperial.Medieval.MobRiding
             component.Rider = buckle.Owner;
             component.IsRiding = true;
             _mover.SetRelay(buckle.Owner, uid);
-            if (CheckPike(buckle.Owner, out var pike))
-            {
-                CreatePikeFixture(uid, component);
-                component.Pike = pike;
-            }
 
             var ev = new StartRideEvent(strap, buckle);
             RaiseLocalEvent(uid, ref ev);
@@ -495,8 +186,6 @@ namespace Content.Shared.Imperial.Medieval.MobRiding
             if (!component.Rider.HasValue)
                 return;
             RemComp<RelayInputMoverComponent>(component.Rider.Value);
-            RemovePikeFixture(uid, component);
-            component.Pike = null;
 
             component.Rider = null;
             component.IsRiding = false;
