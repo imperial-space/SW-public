5284afb02c1e854d97da7d65b7e5c436802a8cc6
FaDeOkno <logkedr18@gmail.com>
2025-08-09 02:07:05 +0400

    fix(src): Фикс сериализации инфы о симптоме

--- Content.Shared/Imperial/Medieval/Plague/DataStoring/MedievalPlagueSymptomData.cs
+++ Content.Shared/Imperial/Medieval/Plague/DataStoring/MedievalPlagueSymptomData.cs
@@ -1,7 +1,9 @@
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.Imperial.Medieval.Plague;
 
+[Serializable, NetSerializable]
 public sealed class MedievalPlagueSymptomData
 {
     public int Points = 0;
