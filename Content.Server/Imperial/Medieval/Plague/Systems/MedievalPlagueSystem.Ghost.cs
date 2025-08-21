7dd03c436dfe001a64b79e2abdd508d7bc8716e6
FaDeOkno <logkedr18@gmail.com>
2025-08-21 07:06:18 +0400

    wip(src): Акшны пиризраке

--- Content.Server/Imperial/Medieval/Plague/Systems/MedievalPlagueSystem.Ghost.cs
+++ Content.Server/Imperial/Medieval/Plague/Systems/MedievalPlagueSystem.Ghost.cs
@@ -1,5 +1,10 @@
 using System.Linq;
+using Content.Server.Body.Components;
+using Content.Shared.Bed.Sleep;
+using Content.Shared.Chemistry.Components;
+using Content.Shared.Damage;
 using Content.Shared.Imperial.Medieval.Plague;
+using Robust.Shared.Random;
 
 namespace Content.Server.Imperial.Medieval.Plague;
 
@@ -9,6 +14,19 @@ public sealed partial class MedievalPlagueSystem
     {
         SubscribeLocalEvent<MedievalPlagueGhostComponent, ComponentInit>(OnGhostInit);
         SubscribeLocalEvent<MedievalPlagueGhostComponent, InfectTargetActionEvent>(OnInfectAction);
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueForcedVomitActionEvent>(OnVomitAction);
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueAsthmaticActionEvent>(OnAsthmaAction);
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueDizzinessActionEvent>(OnDizzinessAction);
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueSleepyActionEvent>(OnSleepAction);
+
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueTeleportInfectedActionEvent>(OnTeleportInfectedAction);
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueTeleportNotInfectedActionEvent>(OnTeleportNotInfectedAction);
+
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueSpawnEntityActionEvent>(OnSpawnEntityAction);
+
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueInjuryActionEvent>(OnInjuryAction);
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueCataractActionEvent>(OnCataractAction);
+        SubscribeLocalEvent<MedievalPlagueGhostComponent, PlagueHeartAttackActionEvent>(OnHeartAttackAction);
     }
 
     private void OnGhostInit(EntityUid uid, MedievalPlagueGhostComponent comp, ComponentInit args)
@@ -42,10 +60,212 @@ public sealed partial class MedievalPlagueSystem
         {
             comp.Points -= cost;
             Dirty(uid, comp);
+            _alerts.ShowAlert(uid, comp.AlertId);
+            UpdateUi(uid);
         }
 
-        var success = TryInfect(args.Target, uid);
-        _popup.PopupEntity(Loc.GetString("medieval-plague-infected-popup", ("success", success)), args.Target, uid);
+        if (TryInfect(args.Target, uid))
+            _popup.PopupEntity(Loc.GetString("medieval-plague-infected-success-popup"), args.Target, uid);
+        else
+            _popup.PopupEntity(Loc.GetString("medieval-plague-infected-failure-popup"), args.Target, uid);
+    }
+
+    private void OnVomitAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueForcedVomitActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!TryUseAbility(uid, args.Target, args.Cost, args.AllowIncubation))
+            return;
+
+        args.Handled = true;
+
+        _vomit.Vomit(args.Target);
+
+        if (_symptoms.Where(x => x.Value.Unlocked).ToDictionary().ContainsKey("BloodVomit") && TryComp<BloodstreamComponent>(uid, out var bloodstream))
+        {
+            Solution sol = new(bloodstream.BloodReagent, 20f);
+            _puddle.TrySpillAt(Transform(uid).Coordinates, sol, out _, false);
+            DamageSpecifier damage = new()
+            {
+                DamageDict = new()
+                {
+                    { "Bloodloss", 10 }
+                }
+            };
+
+            _damageable.TryChangeDamage(args.Target, damage, true);
+        }
+
+        _popup.PopupEntity(Loc.GetString("medieval-plague-forced-vomit-popup"), args.Target, uid);
+    }
+
+    private void OnAsthmaAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueAsthmaticActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!TryUseAbility(uid, args.Target, args.Cost, args.AllowIncubation))
+            return;
+
+        args.Handled = true;
+
+        var block = EnsureComp<PlagueBlockBreathingComponent>(uid);
+        block.EndTime = _timing.CurTime + TimeSpan.FromSeconds(25);
+        _popup.PopupEntity(Loc.GetString("medieval-plague-asthma-ghost-popup"), args.Target, uid);
+        _popup.PopupEntity(Loc.GetString("medieval-plague-asthma-target-popup"), args.Target, args.Target, Shared.Popups.PopupType.MediumCaution);
+    }
+
+    private void OnDizzinessAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueDizzinessActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!TryUseAbility(uid, args.Target, args.Cost, args.AllowIncubation))
+            return;
+
+        args.Handled = true;
+
+        var block = EnsureComp<PlagueDizzinessComponent>(uid);
+        block.EndTime = _timing.CurTime + TimeSpan.FromSeconds(15);
+        _moveSpeed.RefreshMovementSpeedModifiers(args.Target);
+        _popup.PopupEntity(Loc.GetString("medieval-plague-dizziness-ghost-popup"), args.Target, uid);
+        _popup.PopupEntity(Loc.GetString("medieval-plague-dizziness-target-popup"), args.Target, args.Target, Shared.Popups.PopupType.MediumCaution);
+    }
+
+    private void OnSleepAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueSleepyActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!TryUseAbility(uid, args.Target, args.Cost, args.AllowIncubation))
+            return;
+
+        args.Handled = true;
+
+        _status.TryAddStatusEffect<ForcedSleepingComponent>(args.Target, "ForcedSleep", TimeSpan.FromSeconds(15), true);
+        _popup.PopupEntity(Loc.GetString("medieval-plague-forced-sleep-ghost-popup"), args.Target, uid);
+        _popup.PopupEntity(Loc.GetString("medieval-plague-forced-sleep-target-popup"), args.Target, args.Target, Shared.Popups.PopupType.MediumCaution);
+    }
+
+    private void OnTeleportInfectedAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueTeleportInfectedActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        var targets = EntityManager.AllEntities<MedievalPlagueInfectedComponent>();
+        if (!targets.Any())
+        {
+            _popup.PopupEntity(Loc.GetString("medieval-plague-teleport-infected-none-popup"), uid, uid);
+            return;
+        }
+
+        args.Handled = true;
+        _transform.SetCoordinates(uid, Transform(_random.Pick(targets.ToList())).Coordinates);
+    }
+
+    private void OnTeleportNotInfectedAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueTeleportNotInfectedActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        var targets = EntityManager.AllEntities<MedievalCanBeInfectedComponent>().Where(x => !HasComp<MedievalPlagueInfectedComponent>(x.Owner));
+        if (!targets.Any())
+        {
+            _popup.PopupEntity(Loc.GetString("medieval-plague-teleport-not-infected-none-popup"), uid, uid);
+            return;
+        }
+
+        args.Handled = true;
+        _transform.SetCoordinates(uid, Transform(_random.Pick(targets.ToList())).Coordinates);
+    }
+
+    private void OnSpawnEntityAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueSpawnEntityActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (comp.Points < args.Cost)
+        {
+            _popup.PopupCoordinates(Loc.GetString("popup-plague-action-fail-cost"), args.Target, uid);
+            return;
+        }
+
+        comp.Points -= args.Cost;
+        Dirty(uid, comp);
+        _alerts.ShowAlert(uid, comp.AlertId);
+        UpdateUi(uid);
+
+        args.Handled = true;
+        Spawn(args.Prototype, args.Target);
+    }
+
+    private void OnInjuryAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueInjuryActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!TryUseAbility(uid, args.Target, args.Cost, args.AllowIncubation))
+            return;
+
+        args.Handled = true;
+
+        _blood.TryModifyBleedAmount(args.Target, 7.5f);
+    }
+
+    private void OnCataractAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueCataractActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!TryUseAbility(uid, args.Target, args.Cost, args.AllowIncubation))
+            return;
+
+        args.Handled = true;
+
+        _blind.SetMinDamage(args.Target, 3);
+    }
+
+    private void OnHeartAttackAction(EntityUid uid, MedievalPlagueGhostComponent comp, PlagueHeartAttackActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!TryUseAbility(uid, args.Target, args.Cost, args.AllowIncubation))
+            return;
+
+        args.Handled = true;
+
+        _jitter.DoJitter(args.Target, TimeSpan.FromSeconds(4), true, frequency: 6);
+        _mobState.ChangeMobState(args.Target, Shared.Mobs.MobState.Dead);
+    }
+
+    private bool TryUseAbility(EntityUid uid, EntityUid target, int cost = 0, bool allowIncubation = false)
+    {
+        if (!TryComp<MedievalPlagueGhostComponent>(uid, out var comp) || !TryComp<MedievalPlagueInfectedComponent>(target, out var infected))
+        {
+            _popup.PopupEntity(Loc.GetString("popup-plague-action-fail-not-infected"), target, uid);
+            return false;
+        }
+
+        if (infected.Incubation && !allowIncubation)
+        {
+            _popup.PopupEntity(Loc.GetString("popup-plague-action-fail-incubation"), target, uid);
+            return false;
+        }
+
+        if (comp.Points < cost)
+        {
+            _popup.PopupEntity(Loc.GetString("popup-plague-action-fail-cost"), target, uid);
+            return false;
+        }
+
+        comp.Points -= cost;
+        Dirty(uid, comp);
+        _alerts.ShowAlert(uid, comp.AlertId);
+        UpdateUi(uid);
+
+        return true;
     }
 
     private int GetInfectionCost()
--- Content.Shared/Imperial/Medieval/Plague/Events/PlagueActionEvents.cs
+++ Content.Shared/Imperial/Medieval/Plague/Events/PlagueActionEvents.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Actions;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Imperial.Medieval.Plague;
 
@@ -6,3 +7,41 @@ public sealed partial class OpenPlagueEvolutionMenuActionEvent : InstantActionEv
 
 public sealed partial class InfectTargetActionEvent : EntityTargetActionEvent;
 
+public sealed partial class PlagueTeleportInfectedActionEvent : InstantActionEvent;
+
+public sealed partial class PlagueTeleportNotInfectedActionEvent : InstantActionEvent;
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class BasePlagueActionEvent : EntityTargetActionEvent
+{
+    [DataField]
+    public int Cost = 0;
+
+    [DataField]
+    public bool AllowIncubation = false;
+}
+
+public sealed partial class PlagueForcedVomitActionEvent : BasePlagueActionEvent;
+
+public sealed partial class PlagueAsthmaticActionEvent : BasePlagueActionEvent;
+
+public sealed partial class PlagueDizzinessActionEvent : BasePlagueActionEvent;
+
+public sealed partial class PlagueSleepyActionEvent : BasePlagueActionEvent;
+
+public sealed partial class PlagueInjuryActionEvent : BasePlagueActionEvent;
+
+public sealed partial class PlagueCataractActionEvent : BasePlagueActionEvent;
+
+public sealed partial class PlagueHeartAttackActionEvent : BasePlagueActionEvent;
+
+public sealed partial class PlagueBreakImmunityActionEvent : BasePlagueActionEvent;
+
+public sealed partial class PlagueSpawnEntityActionEvent : WorldTargetActionEvent
+{
+    [DataField]
+    public EntProtoId Prototype = string.Empty;
+
+    [DataField]
+    public int Cost = 0;
+}
