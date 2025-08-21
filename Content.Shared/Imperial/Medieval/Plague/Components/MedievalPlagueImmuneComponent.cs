13b99672445e68675aaa59584e44bbe321638dad
FaDeOkno <logkedr18@gmail.com>
2025-08-21 07:03:04 +0400

    add(src): Компонент иммунитета

---
+++ Content.Shared/Imperial/Medieval/Plague/Components/MedievalPlagueImmuneComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Imperial.Medieval.Plague;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class MedievalPlagueImmuneComponent : Component
+{
+    public TimeSpan StartTime;
+}
