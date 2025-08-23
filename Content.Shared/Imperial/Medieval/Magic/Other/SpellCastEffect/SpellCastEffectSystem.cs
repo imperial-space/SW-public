403480b96e9a7f9a73b54f929c7ae928bad71f8a
BiNNiBuX <binnibux@gmail.com>
2025-08-23 22:15:04 +0300

    Фикс бесконечного спавна/Эффект фаербола Т1

--- Content.Shared/Imperial/Medieval/Magic/Other/SpellCastEffect/SpellCastEffectSystem.cs
+++ Content.Shared/Imperial/Medieval/Magic/Other/SpellCastEffect/SpellCastEffectSystem.cs
@@ -5,22 +5,36 @@ namespace Content.Shared.Imperial.Medieval.Magic.SpellCastEffect;
 public sealed partial class EffectSystem : EntitySystem
 {
     [Dependency] private readonly SharedTransformSystem _transform = default!;
-
+    [Dependency] private readonly INetManager _net = default!;
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<SpellCastEffectComponent, MedievalBeforeCastSpellEvent>(OnBeforeCast);
+        SubscribeLocalEvent<SpellCastEffectComponent, MedievalAfterCastSpellEvent>(OnAfterCast);
     }
 
     private void OnBeforeCast(EntityUid uid, SpellCastEffectComponent component, ref MedievalBeforeCastSpellEvent args)
     {
-        if (component.EffectProto != "")
+        if ((component.EffectProto != "") && CanSpawn && _net.IsServer)
         {
             var Effect = Spawn(component.EffectProto, Transform(args.Performer).Coordinates);
             _transform.SetParent(Effect, args.Performer);
+            CanSpawn = false;
         }
         return;
     }
 
+    private void OnAfterCast(EntityUid uid, SpellCastEffectComponent component, ref MedievalAfterCastSpellEvent args)
+    {
+        if (_net.IsServer)
+        {
+            CanSpawn = true;
+        }
+        return;
+    }
+
+    #region Helpers
+    private bool CanSpawn = true;
+    #endregion
 }
