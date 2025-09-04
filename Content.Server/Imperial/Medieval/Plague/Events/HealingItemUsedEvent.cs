0f729798676bb54d031f28f738207c125ccf16f1
FaDeOkno <logkedr18@gmail.com>
2025-09-04 21:09:28 +0400

    fix(src): фикс фикса

--- Content.Server/Imperial/Medieval/Plague/Events/HealingItemUsedEvent.cs
+++ Content.Server/Imperial/Medieval/Plague/Events/HealingItemUsedEvent.cs
@@ -1,4 +1,4 @@
 namespace Content.Server.Imperial.Medieval.Plague;
 
 [ByRefEvent]
-public record struct PlagueHealingItemUsedEvent(int PlagueDecay);
+public record struct PlagueHealingItemUsedEvent(float PlagueDecay);
