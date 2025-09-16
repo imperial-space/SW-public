be8824bae4181c5a4f9e88d56e4cadc432df617a
jojolore123@gmail.com <jojolore123@gmail.com>
2025-09-17 00:17:27 +0300

    Изменение способа ввода фракции

--- Content.Server/Imperial/FactionChange/FactionChangeCommand.cs
+++ Content.Server/Imperial/FactionChange/FactionChangeCommand.cs
@@ -13,16 +13,18 @@ namespace Content.Server.Imperial.FactionChange
     public sealed class FactionChangeCommand : IConsoleCommand
     {
         [Dependency] private readonly IEntityManager _entManager = default!;
+        [Dependency] private readonly IPrototypeManager _proto = default!;
 
         public string Command => "changefaction";
-        public string Description => "Change entity faction to another faction";
-        public string Help => "Usage: changefaction <entity uid> <faction that you want to change(must be a number)>";
+        public string Description => "changefaction-desc";
+        public string Help => "changefaction-help";
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             if (args.Length != 2)
             {
                 shell.WriteError(Loc.GetString("cf-shell-wrong-arguments-number"));
+                shell.WriteError(Loc.GetString(Help));
                 return;
             }
 
@@ -31,50 +33,23 @@ namespace Content.Server.Imperial.FactionChange
                 shell.WriteError(Loc.GetString("cf-shell-entity-uid-must-be-number"));
                 return;
             }
-
-            if (!NetEntity.TryParse(args[1], out var changedFaction))
-            {
-                shell.WriteError(Loc.GetString("cf-shell-entity-faction-must-be-number"));
-                return;
-            }
+            var changedFaction = args[1];
 
             var entityId = _entManager.GetEntity(entityIdNet);
-            var _npcFactionSystem = _entManager.System<NpcFactionSystem>(); // По совету
-            string[] factions =
-            { "NanoTrasen", // 0. НТ. Все гуманоиды по умолчанию имеют эту фракцию
-              "Syndicate", // 1. Синдикат
-              "Dragon", // 2. Дракон и его выводок
-              "Mouse", // 3. Мыши
-              "Passive", // 4. Пассивные существа, что неагрессивны вобще ни к одному существу
-              "PetsNT", // 5. Питомцы станции
-              "SimpleHostile", // 6. Простые агрессивные мобы
-              "SimpleNeutral", // 7. Мобы, что нейтральны почти ко всем
-              "Xeno", // 8. Ксеносы
-              "Zombie", // 9. Зомби
-              "Revolutionary", // 10. Главы революции и обращенные революционеры
-              "AllHostile", // 11. Враждебные ко всем мобы
-              "Wizard", // 12. Маг
-              "Xenoborg", // 13. Ксеноборги
-              "DeathSquad", // 14. Эскадрон Смерти
-              "RiotControlUnit", // 15. РКУ
-              "SyndicateAgent", // 16. Синдикат, но агент, чтобы туррели в ИИ не стреляли по агентам
-              // TODO: Прошу людей с доступом в приват добавить сюда фракции из закрытой сборки
-            };
-
-            var numberCheck = (int)changedFaction;
-            if ((numberCheck > 16) || (numberCheck < 0))
-            {
-                shell.WriteError(Loc.GetString("cf-shell-entity-faction-incorrect-range"));
-                return;
-            }
-
             if (_entManager.HasComponent<NpcFactionMemberComponent>(entityId))
             {
                 var target = entityId;
-                var factionNumber = (int)changedFaction;
-                _npcFactionSystem.ClearFactions(target, true);
-                _npcFactionSystem.AddFaction(target, factions[factionNumber], false);
-                shell.WriteLine(Loc.GetString("cf-command-enter-successful"));
+                var faction = changedFaction;
+                var npcFactionSystem = _entManager.System<NpcFactionSystem>();
+
+                if (!_proto.HasIndex<NpcFactionPrototype>(faction))
+                {
+                    shell.WriteError(Loc.GetString("cf-shell-faction-doesnt-exist", ("faction", faction)));
+                    return;
+                }
+                npcFactionSystem.ClearFactions(target, true);
+                npcFactionSystem.AddFaction(target, faction, false);
+                shell.WriteLine(Loc.GetString("cf-command-enter-successful", ("faction", faction)));
             }
             else
             {
