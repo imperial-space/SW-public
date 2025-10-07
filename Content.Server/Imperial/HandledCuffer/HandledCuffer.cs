3bc0459edd2eeaefcb1ec34affd01428ff63bf90
LightLava <160842994+LightLava@users.noreply.github.com>
2025-10-07 16:44:18 +0700

    фикс

--- Content.Server/Imperial/HandledCuffer/HandledCuffer.cs
+++ Content.Server/Imperial/HandledCuffer/HandledCuffer.cs
@@ -24,7 +24,7 @@ public sealed class HandledCufferSystem : EntitySystem
         if (args.Target is not { Valid: true } target)
             return;
 
-        if(!HasComp<SleepingComponent>(target) || !HasComp<StunnedComponent>(target))
+        if(!HasComp<SleepingComponent>(target) && !HasComp<StunnedComponent>(target))
             return;
 
         if (!args.CanReach)
