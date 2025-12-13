d2f4d9d4b2f76e2b9497811c24a9ce46bb16c5f1
dantanat1 <93005892+dantanat1@users.noreply.github.com>
2025-12-14 02:48:41 +0300

    неотталкиваемость

---
+++ Content.Shared/Imperial/Medieval/MedievalUnthrowableComponent.cs
@@ -0,0 +1,18 @@
+using Content.Shared.Whitelist;
+using Robust.Shared.Utility;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Prototypes;
+using System.Threading;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Content.Shared.Damage;
+
+namespace Content.Shared.TDMNaming.Components
+{
+    [RegisterComponent, NetworkedComponent]
+    public sealed partial class MedievalUnthrowableComponent : Component
+    {
+
+
+    }
+}
