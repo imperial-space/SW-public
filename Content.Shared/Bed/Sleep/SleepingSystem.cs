e89719f14632af8aee5ebc9b799fb24927f2a949
Magic0stick <77047717+Magic0stick@users.noreply.github.com>
2026-01-02 22:16:43 +0300

    Revert "Не удобно спать в броне и шапке"
    This reverts commit 4b14565df3b4d99a2db0f6e07d36fa0e8bdf2a83.

--- Content.Shared/Bed/Sleep/SleepingSystem.cs
+++ Content.Shared/Bed/Sleep/SleepingSystem.cs
@@ -8,7 +8,6 @@ using Content.Shared.Emoting;
 using Content.Shared.Examine;
 using Content.Shared.Eye.Blinding.Systems;
 using Content.Shared.IdentityManagement;
-using Content.Shared.Imperial.Medieval.Skills;
 using Content.Shared.Interaction;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Mobs;
@@ -140,9 +139,6 @@ public sealed partial class SleepingSystem : EntitySystem
         RaiseLocalEvent(ent, ref ev);
         _blindableSystem.UpdateIsBlind(ent.Owner);
         _actionsSystem.AddAction(ent, ref ent.Comp.WakeAction, WakeActionId, ent);
-        if (TryComp<SkillsComponent>(ent, out var skills))
-            _actionsSystem.SetCooldown(ent.Comp.WakeAction, TimeSpan.FromSeconds(10 - (skills.Levels[SharedSkillsSystem.VitalityId]-10) * 0.5));
-
     }
 
     private void OnSpeakAttempt(Entity<SleepingComponent> ent, ref SpeakAttemptEvent args)
