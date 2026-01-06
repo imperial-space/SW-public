3221826fa1206b8f55de5ae0f31ed506158a6860
BiNNiBuX <binnibux@gmail.com>
2026-01-06 06:38:10 +0300

    Ветка земли P3

--- Content.Server/Imperial/Medieval/Magic/Other/Spells/MedievalRandomOffsetSpawn/RandomOffsetSpawnComponent.cs
+++ Content.Server/Imperial/Medieval/Magic/Other/Spells/MedievalRandomOffsetSpawn/RandomOffsetSpawnComponent.cs
@@ -22,8 +22,15 @@ public sealed partial class RandomOffsetSpawnComponent : BaseXOnTriggerComponent
     [DataField(required: true)]
     public int Radius;
     /// <summary>
+    /// Is it necessary to regenerate the coordinates if the coordinates of the already spawned entities are repeated?
+    /// </summary>
+    [DataField]
+    public bool NeedUniqueСoords = true;
+    /// <summary>
     /// Quantity of spawned entities
     /// </summary>
     [DataField(required: true)]
     public int Quantity;
+    [ViewVariables]
+    public List<EntityUid> SpawnedEntitiesUID = new();
 }
--- Content.Server/Imperial/Medieval/Magic/Other/Spells/MedievalRandomOffsetSpawn/RandomOffsetSpawnSystem.cs
+++ Content.Server/Imperial/Medieval/Magic/Other/Spells/MedievalRandomOffsetSpawn/RandomOffsetSpawnSystem.cs
@@ -11,6 +11,7 @@ using Robust.Shared.Physics.Components;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 using Robust.Shared.Random;
+using System.ComponentModel;
 
 namespace Content.Shared.Trigger.Components.Effects;
 
@@ -18,7 +19,7 @@ public sealed partial class RandomOffsetSpawnSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly TransformSystem _transform = default!;
-    [Dependency] private readonly EntityLookupSystem _lookupSystem = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -42,11 +43,30 @@ public sealed partial class RandomOffsetSpawnSystem : EntitySystem
     private void SpawnOnTrigger(EntityUid uid, RandomOffsetSpawnComponent component)
     {
         var coords = _transform.GetMapCoordinates(uid);
+        Vector2 randomVector;
+        Vector2 randomDirectionFromAngle;
+        MapCoordinates resultCoords;
+        bool needNewCoords;
         for (var i = 0; i < component.Quantity; i++)
         {
-            var randomVector = new Vector2(_random.Next(-component.Radius, component.Radius+1), _random.Next(-component.Radius, component.Radius+1));
-            var resultCoords = new MapCoordinates(coords.Position + randomVector, coords.MapId);
+            if (component.NeedUniqueСoords)
+                do
+                {
+                    needNewCoords = false;
+                    randomVector = _random.NextVector2() * _random.Next(component.Radius + 1);
+                    resultCoords = new MapCoordinates(coords.Position + randomVector, coords.MapId);
+                    foreach (var entityNear in _lookup.GetEntitiesInRange(resultCoords, 0.1f))
+                        if (component.SpawnedEntitiesUID.Contains(entityNear))
+                            needNewCoords = true;
+                } while (needNewCoords);
+            else
+            {
+                randomVector = new Vector2(_random.Next(-component.Radius, component.Radius+1), _random.Next(-component.Radius, component.Radius+1));
+                resultCoords = new MapCoordinates(coords.Position + randomVector, coords.MapId);
+            }
             var entityClone = Spawn(component.SpawnedEntityID, resultCoords);
+            component.SpawnedEntitiesUID.Add(entityClone);
         }
     }
 }
+
