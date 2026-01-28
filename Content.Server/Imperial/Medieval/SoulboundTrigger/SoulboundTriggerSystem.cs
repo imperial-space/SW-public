2acc78d4003fb3abb6809586c1a7b79523b91a6c
Zakvielian <daniil.ganin.2004@gmail.com@>
2026-01-28 19:10:46 +0300

    Уменьшил силу взрыва на броне и шлеме архимага до 60 урона (20 ушибов, 20 уколов, 20 ожогов). Исправил комментарии в компоненте

--- Content.Server/Imperial/Medieval/SoulboundTrigger/SoulboundTriggerSystem.cs
+++ Content.Server/Imperial/Medieval/SoulboundTrigger/SoulboundTriggerSystem.cs
@@ -8,18 +8,6 @@ using Content.Server.Clothing.Components;
 using Content.Shared.Trigger.Components;
 
 namespace Content.Server.Imperial.Medieval.SoulboundTrigger;
-
-/// <summary>
-/// Компонент, который привязываезывается к UID сущности, что наденет этот предмет.
-/// Если другой пользователь попытается его надеть, сработает триггер с ключом KeyOut.
-/// </summary>
-/// <remarks>
-/// Если одеть "пустую" одежду через агост, то одежда не привяжется ни к кому. Так работало на локалке.
-/// Но если одеть "привязанную" одежду через агост, то вызовется триггер, если это был не владелец.
-///
-/// Одевание одежды другим игроком так же привязывает её к новому владельцу.
-/// </remarks>
-
 public sealed partial class SoulboundTriggerSystem : EntitySystem
 {
     [Dependency] private readonly ContainerSystem _containerSystem = default!;
