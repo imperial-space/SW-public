ff85ba8f2554cfb8edd0a995f5921ea8489a6513
Inconnu <177014427+Inconnu1337@users.noreply.github.com>
2026-02-07 00:57:33 +0200

    fix pipe connect

--- Content.Server/Imperial/Medieval/Power/Nodes/MyrmexDeviceNode.cs
+++ Content.Server/Imperial/Medieval/Power/Nodes/MyrmexDeviceNode.cs
@@ -30,8 +30,11 @@ public partial class MyrmexDeviceNode : Node
             if (dir == Direction.Invalid)
                 continue;
 
-            if ((ConnectDirections & dir.AsFlag()) != 0 && node is MyrmexPipeNode)
-                yield return node;
+            if ((ConnectDirections & dir.AsFlag()) != 0 && node is MyrmexPipeNode pipeNode)
+            {
+                if (CanPipeConnectFromDirection(pipeNode, dir.GetOpposite(), entMan, xformQuery))
+                    yield return node;
+            }
         }
 
 
@@ -40,5 +43,53 @@ public partial class MyrmexDeviceNode : Node
             if (node != this && node is MyrmexPipeNode)
                 yield return node;
         }
+    }
+
+    private bool CanPipeConnectFromDirection(MyrmexPipeNode pipeNode, Direction dirFromPipe,
+        IEntityManager entMan, EntityQuery<TransformComponent> xformQuery)
+    {
+        if (!entMan.TryGetComponent<MyrmexPipeComponent>(pipeNode.Owner, out var pipeComp))
+            return false;
+
+        if (!xformQuery.TryGetComponent(pipeNode.Owner, out var pipeXform))
+            return false;
+
+        var pipeRotation = pipeXform.LocalRotation.GetCardinalDir();
+        var allowedDirections = GetAllowedDirections(pipeComp.PipeType, pipeRotation);
+
+        return allowedDirections.Contains(dirFromPipe);
+    }
+
+    private HashSet<Direction> GetAllowedDirections(MyrmexPipeType pipeType, Direction rotation)
+    {
+        var directions = new HashSet<Direction>();
+
+        switch (pipeType)
+        {
+            case MyrmexPipeType.Straight:
+                directions.Add(rotation);
+                directions.Add(rotation.GetOpposite());
+                break;
+
+            case MyrmexPipeType.Corner:
+                directions.Add(rotation);
+                directions.Add(rotation.GetClockwise90Degrees());
+                break;
+
+            case MyrmexPipeType.TJunction:
+                directions.Add(rotation);
+                directions.Add(rotation.GetOpposite());
+                directions.Add(rotation.GetClockwise90Degrees());
+                break;
+
+            case MyrmexPipeType.Cross:
+                directions.Add(Direction.North);
+                directions.Add(Direction.South);
+                directions.Add(Direction.East);
+                directions.Add(Direction.West);
+                break;
+        }
+
+        return directions;
     }
 }
