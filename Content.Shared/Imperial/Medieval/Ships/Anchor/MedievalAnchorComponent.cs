19397e61a0ca363be4fd1c9acded64a4f4b8d5ae
Maxwell1007 <maxsbel@gmail.com>
2026-05-07 20:40:15 +0300

    Переход на general visualiser

--- Content.Shared/Imperial/Medieval/Ships/Anchor/MedievalAnchorComponent.cs
+++ Content.Shared/Imperial/Medieval/Ships/Anchor/MedievalAnchorComponent.cs
@@ -8,21 +8,9 @@ public enum MedievalAnchorVisuals : byte
     Enabled
 }
 
-[Serializable, NetSerializable]
-public enum MedievalAnchorVisualLayers : byte
-{
-    Base
-}
-
 [RegisterComponent]
 public sealed partial class MedievalAnchorComponent : Component
 {
     [DataField("Enabled")]
     public bool Enabled;
-
-    [DataField("upState")]
-    public string UpState = "up";
-
-    [DataField("downState")]
-    public string DownState = "down";
 }
