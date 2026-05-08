466200af8ad8fbbe011dcb01c4bbbed2ff8c3627
Maxwell1007 <maxsbel@gmail.com>
2026-05-08 12:13:25 +0300

    Привести утопление в порядок

--- Content.Server/Imperial/Medieval/Ships/PlayerDrowning/PlayerDrowningComponent.cs
+++ Content.Server/Imperial/Medieval/Ships/PlayerDrowning/PlayerDrowningComponent.cs
@@ -1,17 +1,25 @@
+using Content.Shared.Damage;
+
 namespace Content.Server.Imperial.Medieval.Ships.PlayerDrowning;
 
-/// <summary>
-/// This is used for...
-/// </summary>
 [RegisterComponent]
 public sealed partial class PlayerDrowningComponent : Component
 {
     [DataField("drownTime")]
     public int DrownTime;
 
+    [DataField("maxDrownTime")]
+    public int MaxDrownTime = 25;
+
     [DataField("speedModifier")]
     public float SpeedModifier = 0.5f;
 
-    [DataField("Undrowable")]
-    public bool Undrowable;
+    [DataField("drowningDamage")]
+    public DamageSpecifier DrowningDamage = new()
+    {
+        DamageDict = new()
+        {
+            { "Asphyxiation", 10 }
+        }
+    };
 }
