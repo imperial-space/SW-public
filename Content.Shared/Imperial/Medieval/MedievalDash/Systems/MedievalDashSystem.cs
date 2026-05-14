10c77c7c2a488e74ca222f27d42d19b240fcf4e0
Zakvielian <daniil.ganin.2004@gmail.com>
2026-05-14 17:18:33 +0300

    Теперь пройти через стену дэшом нельзя

--- Content.Shared/Imperial/Medieval/MedievalDash/Systems/MedievalDashSystem.cs
+++ Content.Shared/Imperial/Medieval/MedievalDash/Systems/MedievalDashSystem.cs
@@ -194,7 +194,7 @@ public sealed partial class MedievalDashSystem : EntitySystem
             foreach (var result in results)
             {
                 if (result.HitEntity != EntityUid.Invalid)
-                    return result.Distance;
+                    return Math.Max(0, result.Distance - 0.75f);
             }
         }
 
