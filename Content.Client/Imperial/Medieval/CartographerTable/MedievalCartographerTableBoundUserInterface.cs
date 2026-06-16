f5b403759a239e845248297774ad6b69e1138f59
Maxwell1007 <maxsbel@gmail.com>
2026-06-16 19:32:53 +0300

    фикс звуков карты(фл)

--- Content.Client/Imperial/Medieval/CartographerTable/MedievalCartographerTableBoundUserInterface.cs
+++ Content.Client/Imperial/Medieval/CartographerTable/MedievalCartographerTableBoundUserInterface.cs
@@ -1,9 +1,6 @@
 using Content.Shared.Shuttles.BUIStates;
 using JetBrains.Annotations;
-using Robust.Client.GameObjects;
 using Robust.Client.UserInterface;
-using Robust.Shared.Audio.Systems;
-using Robust.Shared.Player;
 
 namespace Content.Client.Imperial.Medieval.CartographerTable;
 
@@ -12,7 +9,6 @@ public sealed class MedievalCartographerTableBoundUserInterface : BoundUserInter
 {
     [ViewVariables]
     private MedievalCartographerTableWindow? _window;
-    private bool _closeSoundPlayed;
 
     public MedievalCartographerTableBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
@@ -21,21 +17,7 @@ public sealed class MedievalCartographerTableBoundUserInterface : BoundUserInter
     protected override void Open()
     {
         base.Open();
-
-        _closeSoundPlayed = false;
-        EntMan.System<SharedAudioSystem>().PlayGlobal("/Audio/Imperial/Medieval/Plague/menu_open.ogg", Filter.Local(), false);
         _window = this.CreateWindow<MedievalCartographerTableWindow>();
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-        base.Dispose(disposing);
-
-        if (disposing && !_closeSoundPlayed)
-        {
-            _closeSoundPlayed = true;
-            EntMan.System<SharedAudioSystem>().PlayGlobal("/Audio/Imperial/Medieval/Plague/menu_close.ogg", Filter.Local(), false);
-        }
     }
 
     protected override void UpdateState(BoundUserInterfaceState state)
