eb94093236452cdcad35f2bb3f7bfad0c1c3298f
BladeFire5 <angelmorel510@gmail.com>
2026-09-26 11:42:16 -0400

    Low pop support

--- Content.Server/Imperial/Medieval/Barrier/MagicBarrierDrainCalculator.cs
+++ Content.Server/Imperial/Medieval/Barrier/MagicBarrierDrainCalculator.cs
@@ -4,23 +4,37 @@ namespace Content.Server.MagicBarrier;
 
 public static class MagicBarrierDrainCalculator
 {
-    public static float Calculate(MagicBarrierComponent comp, int growthCount, int riftCount)
+    public static float Calculate(MagicBarrierComponent comp, int growthCount, int riftCount, int playerCount)
     {
-        var lowCurse =
-            (comp.BaseCurseDrain + comp.RiftCurseDrain * riftCount) *
-            MathF.Pow(
-                comp.MagicBarrierCurseEffect,
-                1f + growthCount);
+        var baseCurseDrain = comp.BaseCurseDrain;
+        var riftCurseDrain = comp.RiftCurseDrain;
+
+        var lowHardCap = comp.HLCurseLimit;
+        var highHardCap = comp.HHCurseLimit;
+
+        if (playerCount < 30)
+        {
+            baseCurseDrain = 0.5f;
+            riftCurseDrain = 1f;
+            lowHardCap = 4f;
+            highHardCap = 4f;
+        }
+        else if (playerCount < 60)
+        {
+            baseCurseDrain = 1f;
+            riftCurseDrain = 2f;
+            lowHardCap = 4f;
+            highHardCap = 8f;
+        }
+
+        var lowCurse = (baseCurseDrain + riftCurseDrain * riftCount) *
+        MathF.Pow(comp.MagicBarrierCurseEffect, 1f + growthCount);
 
         var totalSources = growthCount + riftCount;
 
-        var highCurse = totalSources <= comp.ACurseLimit
-            ? comp.HLCurseLimit
-            : comp.HLCurseLimit +
-              (comp.HHCurseLimit - comp.HLCurseLimit) *
-              (1f - MathF.Exp(
-                  -comp.OCurseRate *
-                  (totalSources - comp.ACurseLimit)));
+        var highCurse = totalSources <= comp.ACurseLimit ? lowHardCap : lowHardCap +
+        (highHardCap - lowHardCap) *
+        (1f - MathF.Exp(-comp.OCurseRate * (totalSources - comp.ACurseLimit)));
 
         return MathF.Round(
             MathF.Min(lowCurse, highCurse),
